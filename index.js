2982c84861469e287bb027f669acee810fd42d51
Carol <carol.tsai0224@gmail.com>
2022-04-11 16:05:24 -0400

    added styles to html, wrote functions to write html in index.js

--- index.js
+++ index.js
@@ -141,20 +141,78 @@ function makeTeam() {
 
 //todo write this
 // Function that takes in an array of Employees and writes an HTML file
-function writeHTML() {
-   return;
+function startHTML() {
+   const data = `
+   <!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" />
+
+   <title>Team Profile</title>
+</head>
+
+<body>
+   <header class="jumbotron jumbotron-fluid bg-dark text-light text-center">
+      <h1>
+         My Team
+      </h1>
+   </header>
+   <section class="container">
+      <section class="row justify-content-center">
+   `
+   fs.appendFile('./dist/team.html', data);
 }
 
 function writeManager(manager) {
-   return;
+   const data = `
+       <div class="card m-2" style="width: 18rem;">
+         <div class="card-body">
+            <h5 class="card-title">${manager.name}</h5>
+            <h6 class = "card-subtitle">Manager</h6>
+            <ul class="list-group list-group-flush">
+               <li class="list-group-item">ID: ${manager.id}</li>
+               <li class="list-group-item">email: ${manager.email}</li>
+               <li class="list-group-item">office number: ${manager.office}</li>
+            </ul>
+         </div>
+      </div>
+   `;
+   fs.appendFile('./dist/team.html', data);
 }
 
 function writeEngineer(engineer) {
-   return;
+   const data = `
+         <div class="card m-2" style="width: 18rem;">
+         <div class="card-body">
+            <h5 class="card-title">${engineer.name}</h5>
+            <h6 class="card-subtitle">Engineer</h6>
+            <ul class="list-group list-group-flush">
+               <li class="list-group-item">ID: ${engineer.id}</li>
+               <li class="list-group-item">email: ${engineer.email}</li>
+               <li class="list-group-item">github: ${engineer.github}</li>
+            </ul>
+         </div>
+      </div>`;
+   fs.appendFile('./dist/team.html', data);
 }
 
 function writeIntern(intern) {
-   return;
+   const data = `      <div class="card m-2" style="width: 18rem;">
+         <div class="card-body">
+            <h5 class="card-title">${intern.name}</h5>
+            <h6 class="card-subtitle">Intern</h6>
+            <ul class="list-group list-group-flush">
+               <li class="list-group-item">ID: ${intern.id}</li>
+               <li class="list-group-item">email: ${intern.email}</li>
+               <li class="list-group-item">school: ${intern.school}</li>
+            </ul>
+         </div>
+      </div>`
+   fs.appendFile('./dist/team.html', data);
 }
 
 function finishHTML() {
@@ -163,8 +221,9 @@ function finishHTML() {
 
 // Runs the application
 function init() {
+   startHTML();
    const team = makeTeam();
-   writeHTML(team);
+   
 }
 
 makeTeam();
